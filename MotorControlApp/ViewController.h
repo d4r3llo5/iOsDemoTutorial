cbe17f386419f08c49917b8ffccfd9148b820b08
iBeacon <ibeacon@rover-96-42.rovernet.mtu.edu>
2014-12-06 15:23:08 -0500

    Initial Commit

---
+++ MotorControlApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MotorControlApp
+//
+//  Created by iBeacon on 12/6/14.
+//  Copyright (c) 2014 MTS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MotorControlApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MotorControlApp
+//
+//  Created by iBeacon on 12/6/14.
+//  Copyright (c) 2014 MTS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
